ed9328e6d97bb9739702e540649c4a0fb9e98a48
Guy <guy.lud@gmail.com>
2017-10-16 23:48:55 +0300

    first option type

---
+++ src/ExistAll.Shepherd.Core/IOptionsValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace ExistAll.Shepherd.Core
+{
+	internal interface IOptionsValidator
+	{
+		void ValidateOptions(IShepherdOptions options);
+	}
+
+	class OptionsValidator : IOptionsValidator
+	{
+		public void ValidateOptions(IShepherdOptions options)
+		{
+			
+		}
+	}
+}
--- src/ExistAll.Shepherd.Core/Shepherd.cs
+++ src/ExistAll.Shepherd.Core/Shepherd.cs
@@ -2,27 +2,33 @@
 {
 	public class Shepherd : IShepherd
 	{
+		private readonly IOptionsValidator _optionsValidator;
 		public AssemblyCollection Assemblies { get; internal set; } = new AssemblyCollection();
 		public ModuleCollection Modules { get; internal set; } = new ModuleCollection();
 		public IShepherdOptions Options { get; internal set; } = new DefaultShepherdOptions();
 
-		internal Shepherd()
+		public Shepherd()
+			: this(new OptionsValidator())
 		{
 			
 		}
 
+		internal Shepherd(IOptionsValidator optionsValidator)
+		{
+			_optionsValidator = optionsValidator;
+		}
+
 		public IServiceResolver Herd()
 		{
+			_optionsValidator.ValidateOptions(Options);
+
 			var allTypes = Assemblies.GetAllTypes();
 
-			Options.
+			var filterTypes = Options.TypeMatcher.FilterTypes(allTypes);
+
+			var typesIndex = Options.TypeMatcher.MapTypes(filterTypes);
 
 			return null;
 		}
-	}
-
-	internal interface IOptionsValidator
-	{
-		
 	}
 }
