43ff8ec8d7936c3430314b941334d2fe1ef8ca41
guy lud <guy.l@walkme.com>
2017-10-22 23:28:46 +0300

    name chanegs

---
+++ src/Core/ExistsForAll.Shepherd.SimpleInjector/IServiceIndexer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExistsForAll.Shepherd.SimpleInjector
+{
+	public interface IServiceIndexer
+	{
+		Predicate<Type> TypeFilter { get; set; }
+		IEnumerable<ServiceTypeMap> MapTypes(IEnumerable<Type> applicationTypes);
+	}
+}
