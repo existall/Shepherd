4b0a5b9bb455ecc32adf39c44b0963a43e8909e3
Guy <guy.lud@gmail.com>
2017-10-24 21:58:29 +0300

    add test

---
+++ src/tests/ExistsForAll.Shepherd.SimpleInjector.UnitTests/CollectionRegistrationBehaviorTests.cs
@@ -0,0 +1,53 @@
+using System;
+using ExistsForAll.Shepherd.SimpleInjector.RegistrationActions;
+using Xunit;
+using static ExistsForAll.Shepherd.SimpleInjector.UnitTests.TestUtils;
+
+namespace ExistsForAll.Shepherd.SimpleInjector.UnitTests
+{
+	public class CollectionRegistrationBehaviorTests
+	{
+		[Fact]
+		public void ShouldRegister_WhenHaveLessThanTwoImpl_ShouldReturnFalse()
+		{
+			var sut = BuildSut();
+			var descriptor = BuildDescriptor(GetType<Interface>());
+
+			var result = sut.ShouldRegister(descriptor);
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ShouldRegister_WhenHaveMoreThanOneImpl_ShouldReturnFalse()
+		{
+			var sut = BuildSut();
+			var descriptor = BuildDescriptor(GetType<Interface>(), GetType<Interface1>());
+
+			var result = sut.ShouldRegister(descriptor);
+			Assert.True(result);
+		}
+
+		private IServiceDescriptor BuildDescriptor(Type type, params Type[] types)
+		{
+			return BuildServiceDescriptor(GetType<IInterface>(), type, types);
+		}
+
+		private static CollectionRegistrationBehavior BuildSut()
+		{
+			return new CollectionRegistrationBehavior();
+		}
+
+		private interface IInterface
+		{
+
+		}
+
+		private class Interface : IInterface
+		{
+		}
+
+		private class Interface1 : IInterface
+		{
+		}
+	}
+}
