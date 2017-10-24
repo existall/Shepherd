6cd9129ae9432fc425427e0fb44e10c6ff0ede61
Guy <guy.lud@gmail.com>
2017-10-25 00:08:13 +0300

    remove comments

--- src/Core/ExistsForAll.Shepherd.SimpleInjector/ModuleContext.cs
+++ src/Core/ExistsForAll.Shepherd.SimpleInjector/ModuleContext.cs
@@ -5,37 +5,6 @@ using SimpleInjector;
 
 namespace ExistsForAll.Shepherd.SimpleInjector
 {
-	//public abstract class Shepherd<T>
-	//{
-	//	private readonly T _container;
-
-	//	//public IServiceRegistrator ServiceRegistrator { get; set; } = new WindsorServiceRegistrator();
-
-	//	protected Shepherd(T container)
-	//	{
-	//		_container = container;
-	//	}
-
-	//	public Options Options { get; }
-
-	//	public IServiceResolver Initialize(AssemblyCollection assemblyCollection, ModuleCollection modules)
-	//	{
-	//		var types = assemblyCollection.GetAllTypes().ToArray();
-	//		var assemblies = assemblyCollection.Assemblies.ToArray();
-
-	//		IInitializingContext<T> context = CreateInitializingContext(_container, types, assemblies);
-
-	//		modules.ForEach(x =>
-	//		{
-	//			//x.Run(context);
-	//		});
-
-	//		return null;
-	//	}
-
-	//	protected abstract IInitializingContext<T> CreateInitializingContext(T container, Type[] types, Assembly[] assemblies);
-	//}
-
 	internal class ModuleContext : IModuleContext
 	{
 		public IEnumerable<Assembly> Assemblies { get; }
