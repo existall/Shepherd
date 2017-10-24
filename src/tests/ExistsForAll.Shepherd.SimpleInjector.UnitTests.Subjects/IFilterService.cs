ed27199c9b0380575bd453f6ae45c3e1519dd6ee
Guy <guy.lud@gmail.com>
2017-10-25 00:05:09 +0300

    fix integration test

--- src/tests/ExistsForAll.Shepherd.SimpleInjector.UnitTests.Subjects/IFilterService.cs
+++ src/tests/ExistsForAll.Shepherd.SimpleInjector.UnitTests.Subjects/IFilterService.cs
@@ -4,4 +4,8 @@
 	{
 		
 	}
+
+	public class FilterService : IFilterService
+	{
+	}
 }
--- src/tests/ExistsForAll.Shepherd.SimpleInjector.UnitTests/ShepherdTests.cs
+++ src/tests/ExistsForAll.Shepherd.SimpleInjector.UnitTests/ShepherdTests.cs
@@ -17,35 +17,43 @@ namespace ExistsForAll.Shepherd.SimpleInjector.UnitTests
 			sut.Options.ServiceIndexer.Filters.Add(new InterfaceAccumulationFilter(typeof(IFilterService)));
 			var container = sut.Herd();
 
-			container.Verify();
-
-			AssertServiceRegistration<DecoratorServiceDecorator>(typeof(IDecoratorService), i =>
+			try
 			{
-				Assert.IsType<DecoratorServiceDecorator>(i);
-				Assert.IsType<DecoratorService>(i.DecoratorService);
-			});
+				container.Verify();
 
-			AssertServiceRegistration<SingleImplService>(typeof(ISingleImplService), i =>
+				AssertServiceRegistration<DecoratorServiceDecorator>(typeof(IDecoratorService), i =>
+				{
+					Assert.IsType<DecoratorServiceDecorator>(i);
+					Assert.IsType<DecoratorService>(i.DecoratorService);
+				});
+
+				AssertServiceRegistration<SingleImplService>(typeof(ISingleImplService), i =>
+				{
+					Assert.IsType<SingleImplService>(i);
+				});
+
+				AssertServiceRegistration<OpenCloseGeneric>(typeof(IOpenCloseGeneric<int>), i =>
+				{
+					Assert.IsType<OpenCloseGeneric>(i);
+					Assert.IsType<int>(i.TypeOfGeneric);
+				});
+
+				var openGenericInstance = container.GetInstance(typeof(IOpenGenerics<int>));
+				Assert.IsType<OpenGenerics<int>>(openGenericInstance);
+
+				var collectionResults = container.GetAllInstances<ICollectionService>();
+				Assert.Collection(collectionResults, i => Assert.IsType<CollectionService1>(i),
+					i => Assert.IsType<CollectionService2>(i),
+					i => Assert.IsType<CollectionService3>(i));
+
+				NotRegisteredAssertion(typeof(IFilterService));
+				NotRegisteredAssertion(typeof(INoImplInterface));
+
+			}
+			finally
 			{
-				Assert.IsType<SingleImplService>(i);
-			});
-
-			AssertServiceRegistration<OpenCloseGeneric>(typeof(IOpenCloseGeneric<int>), i =>
-			{
-				Assert.IsType<OpenCloseGeneric>(i);
-				Assert.IsType<int>(i.TypeOfGeneric);
-			});
-
-			var openGenericInstance = container.GetInstance(typeof(IOpenGenerics<int>));
-			Assert.IsType<OpenGenerics<int>>(openGenericInstance);
-
-			var collectionResults = container.GetAllInstances<ICollectionService>();
-			Assert.Collection(collectionResults, i => Assert.IsType<CollectionService1>(i),
-				i => Assert.IsType<CollectionService2>(i),
-				i => Assert.IsType<CollectionService3>(i));
-
-			NotRegisteredAssertion(typeof(IFilterService));
-			NotRegisteredAssertion(typeof(INoImplInterface));
+				container.Dispose();
+			}
 
 			void AssertServiceRegistration<T>(Type serviceType, Action<T> assertionAction)
 			{
@@ -56,8 +64,10 @@ namespace ExistsForAll.Shepherd.SimpleInjector.UnitTests
 
 			void NotRegisteredAssertion(Type type)
 			{
-				Assert.Throws<ActivationException > (() => container.GetInstance(type));
+				Assert.Throws<ActivationException>(() => container.GetInstance(type));
 			}
+
+
 		}
 	}
 }
