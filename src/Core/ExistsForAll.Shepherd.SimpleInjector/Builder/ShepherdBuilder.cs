02448d2a2409bdf963ae26391de69895191b7d24
Guy <guy.lud@gmail.com>
2018-09-08 19:52:49 +0300

    WIp

---
+++ src/Core/ExistsForAll.Shepherd.SimpleInjector/Builder/ShepherdBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SimpleInjector;
+
+namespace ExistsForAll.Shepherd.SimpleInjector.Builder
+{
+	public class ShepherdBuilder : IOptionsBuilder, IAssembliesBuilder
+	{
+		private Shepherd Shepherd { get; }
+
+		public ShepherdBuilder(Container container)
+		{
+			Shepherd = new Shepherd(container);
+		}
+
+		public object WithOptions(Action<IShepherdOptions> action)
+		{
+			action?.Invoke(Shepherd.Options);
+			return this;
+		}
+
+		public object WithAssemblies(Action<AssemblyCollection> action)
+		{
+			action?.Invoke(Shepherd.Assemblies);
+			return this;
+		}
+	}
+
+	public interface IAssembliesBuilder
+	{
+	}
+
+	public interface IOptionsBuilder
+	{
+		object WithOptions(Action<IShepherdOptions> action);
+	}
+}
---
+++ src/Core/ExistsForAll.Shepherd.SimpleInjector/Builder/SimpleInjectorExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExistsForAll.Shepherd.SimpleInjector.Builder
+{
+	class Class1
+	{
+	}
+}
